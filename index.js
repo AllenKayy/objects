411bc6fad2eb8e20c28096707754a0ffb21f8361
AllenKayy <allisonoyinkansola1@gmail.com>
2023-03-04 14:42:34 +0100

    constructor and factory functions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // question 1
 
+// instagramPost constructor function
 function instagramPost (authorHandle, content, imageLink, views, likes) {
     this.authorHandle = authorHandle;
     this.content = content;
@@ -10,7 +11,8 @@ function instagramPost (authorHandle, content, imageLink, views, likes) {
 
 // question 2
 
-const igPost1 = new instagramPost ('George', "It's a beautiful day", 'https://post1.com/image.jpg', 3000, 2500);
+// create instagramPost objects
+const igPost1 = new instagramPost ('Somto', "It's a beautiful day", 'https://post1.com/image.jpg', 3000, 2500);
 
 const igPost2 = new instagramPost ('Ini', 'Delicious meal at a new restaurant', 'https://post2.com/image.jpg', 4000, 3800);
 
@@ -18,5 +20,39 @@ const igPost2 = new instagramPost ('Ini', 'Delicious meal at a new restaurant', 
 console.log(igPost1);
 console.log(igPost2); 
 
+// question 3
 
+// createPerson() factory function
+function createPerson(name, age, location) {
+    return {
+        name:name,
+        age:age,
+        location:location
+    };
+}
 
+// Create an object representing Musa
+
+const musa = createPerson('Musa Dawodu', 19, 'Lekki, Lagos State')
+console.log(musa); 
+
+// Factory function to create a JAMB scores object
+
+function createJambScores(eng, govt, lit, crk) {
+    return {
+        eng:eng,
+        govt:govt,
+        lit:lit,
+        crk:crk
+    };
+}
+
+// create an object representing Musa’s JAMB scores
+
+const musaJambScores = createJambScores(70, 85, 82, 94)
+console.log(musaJambScores);
+
+// Add the jambscore object as a property to Musa object
+
+musa.jambScores = musaJambScores
+console.log(musa)
